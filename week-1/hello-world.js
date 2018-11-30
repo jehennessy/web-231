bdc1272cf58f09283cdf692448c8705ec9c5d7ad
Jordan Hennessy <jorrdygirl@me.com>
2018-11-30 11:58:20 -0600

    Exercise 1.3 Hellow World commit

---
+++ week-1/hello-world.js
@@ -0,0 +1,11 @@
+/* 
+
+Title: Hello World
+Author: Professor Krasso
+Date: 30 November 2018
+Modified By: <Jordan Hennessy>
+Description: This program demonstrates how to run a Javascript application in VS.
+
+*/
+
+console.log("Hello World, my name is Jordan Hennessy");
