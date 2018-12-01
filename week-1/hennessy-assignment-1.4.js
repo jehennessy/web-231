ec89c53cadf925d5e790cac7d8e9bc043e9c349d
Jordan Hennessy <jorrdygirl@me.com>
2018-12-01 17:04:52 -0600

    Assignment 1.4 commit

---
+++ week-1/hennessy-assignment-1.4.js
@@ -0,0 +1,52 @@
+/* 
+
+Title: Assignment 1.4 Primitive Types
+Author: Professor Krasso
+Date: 30 November 2018
+Modified By: <Jordan Hennessy>
+Description: This program demonstrates the ; use of JavaScript types, values, and
+; and variables in an application.
+
+*/
+
+// start program
+
+/*
+    Expected output:
+    
+    First name
+    Last name
+    Age
+    Street
+    City
+    State 
+    Zip code 
+*/ 
+
+// variable declaration and assignment goes here ...
+var firstName; // first name declaration
+var firstName = "Jordan"; //first name assignment
+var lastName; // last name declaration
+var lastName = "Hennessy"; // last name assignment
+var age; // age declaration
+var age = "26"; // age assignment
+var street; // street declaration
+var street = "1111 Apple Street"; // street assignment
+var city; // city declaration
+var city = "Omaha"; // city assignment
+var state; // state declaration
+var state = "Nebraska"; // state assignment
+var zipCode; // zip code declaration
+var zipCode = "68144"; // zip code assignment
+
+// output 
+console.log(firstName); // output first name
+console.log(lastName); // output last name
+console.log(age); // output age
+console.log(street); // output street
+console.log(city); // output city
+console.log(state); // output state
+console.log(zipCode); // output zip code
+
+
+// end program 
