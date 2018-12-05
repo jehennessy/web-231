d6ce40c048365a6b61e103039bb86cc81bc54d99
Jordan Hennessy <jorrdygirl@me.com>
2018-12-05 17:16:28 -0600

    header and header test commit

---
+++ week-2/hennessy-header-text.js
@@ -0,0 +1,13 @@
+/*
+============================================
+; Title:  header.js
+; Author: Professor Krasso 
+; Date:   5 December 2018
+; Modified By: Jordan Hennessy
+; Description: Displays a formatted header
+;===========================================
+*/
+
+const header = require('../hennessy-header.js');
+
+console.log(header.display("Jordan", "Hennessy", "Exercise 2.2 - Header"));
