9ad92e7a26f0349f99d6c30db076082c2f6ca7bb
Jordan Hennessy <jorrdygirl@me.com>
2018-12-05 19:04:07 -0600

    Exercise 2.3 commit

--- week-2/hennessy-exercise-2.3.js
+++ week-2/hennessy-exercise-2.3.js
@@ -10,19 +10,6 @@
 ;===========================================
 */
 
-/*
-  Expected output:
-
-  FirstName LastName
-  Exercise 2.3
-  Today's Date
-
-  Hello FirstName LastName!
-
-  Hint: Use your personal header display function and
-  refer to page 178 for implementing function properties
-*/
-
 // header
 const header = require('../hennessy-header.js');
 
