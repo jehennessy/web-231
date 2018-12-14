d988ac04b7effdb26396ec19aaeb8fbd859df417
Jordan Hennessy <jorrdygirl@me.com>
2018-12-14 09:08:05 -0600

    Exercise 3.2 commit

---
+++ week-3/hennessy-discussion-3.1.js
@@ -0,0 +1,34 @@
+// start program
+
+/*
+============================================
+; Title:  hennessy-discussion-3.1.js
+; Author: Jordan Hennessy
+; Date:   11 December 2018
+; Modified By: none
+; Description: Create a simple JavaScript program using either a for, while, if or switch statement with at least two errors.
+;===========================================
+*/
+
+
+// header
+const header = require('../hennessy-header.js');
+
+console.log(header.display("Jordan", "Hennessy", "Discussion 3.1"));
+// end header
+
+console.log("\n")
+
+var x = 5;
+
+if (x == 3) { // x is equal to 3
+  console.log("x equals 3");
+}
+else (x > 3) { // x is greater than 3
+  console.log("x is more than 3");
+}
+else (x < 3){ // x is less than 3
+  console.log("x is less than 3");
+}
+
+
