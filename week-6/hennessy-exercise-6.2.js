7dec378e2c7ae81c54ab3d8a9ffa8c87da95e296
Jordan Hennessy <jorrdygirl@me.com>
2019-01-15 20:45:43 -0600

    Exercise 6.2

--- week-6/hennessy-exercise-6.2.js
+++ week-6/hennessy-exercise-6.2.js
@@ -19,23 +19,23 @@ console.log(header.display("Jordan", "Hennessy", "Exercise 6.2"));
 
 console.log("\n")
 
-
+// try/catch/finally statement
 
 try{
 
-  var n = 15;
+  var n = 15; // set variables
 
-  var k = 3;
+  var k = 3; // set variables
 
   var sum = n-k;
 
-if (sum > 10)throw "The sum is higher than 10";
+if (sum > 10)throw "The sum is higher than 10"; // set error message
 
   console.log(sum);
 
 }catch(err){
 
-  console.log("Catch clause: " + err);
+  console.log("Catch clause: " + err); // output error clause
 
 }finally{
 
